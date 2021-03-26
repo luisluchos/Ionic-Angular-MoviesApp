a7146c8f63cc3cd08d265be7f486c229b4728157
Luis Marin <luismgval@gmail.com>
2021-03-26 08:09:41 +0100

    Routes setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,24 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   
-  {
-    path: '',
-    redirectTo: 'movies',
-    pathMatch: 'full'
-  },
+  {path: '',redirectTo: 'movies',pathMatch: 'full'},
+
   {
     path: 'movies',
-    loadChildren: () => import('./pages/movies/movies.module').then( m => m.MoviesPageModule)
-  }
+    children:[
+      {
+        path:'',
+        loadChildren: () => import('./pages/movies/movies.module').then( m => m.MoviesPageModule)
+      },
+      {
+        path:':movieId',
+        loadChildren: () => import('./pages/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+      }
+    ]    
+  },
+
+  { path: '404',  loadChildren: () => import('./pages/page-not-found/page-not-found.module').then( m => m.PageNotFoundPageModule) },
+  { path: '**', redirectTo: '/404', pathMatch: 'full' }
 ];
 
 @NgModule({
